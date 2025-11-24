8544975b945980afa5f9c6802ee75ccce7386658
Arvesh Gosine <arveshgosine@gmail.com>
2025-11-23 22:53:12 -0400

    #12 Implement Login Page

--- App/views/auth.py
+++ App/views/auth.py
@@ -1,38 +1,52 @@
 from flask import Blueprint, render_template, jsonify, request, flash, send_from_directory, flash, redirect, url_for
 from flask_jwt_extended import jwt_required, current_user, unset_jwt_cookies, set_access_cookies
 
-
 from.index import index_views
 
 from App.controllers import (
-    login,
-
+    jwt_authenticate,
+    get_user_by_name
 )
 
 auth_views = Blueprint('auth_views', __name__, template_folder='../templates')
 
 
-
-
-'''
-Page/Action Routes
-'''    
-
-@auth_views.route('/identify', methods=['GET'])
-@jwt_required()
-def identify_page():
-    return render_template('message.html', title="Identify", message=f"You are logged in as {current_user.id} - {current_user.username}")
-    
-
-@auth_views.route('/login', methods=['POST'])
-def login_action():
+@auth_views.route('/login/staff', methods=['POST'])
+def login_staff():
     data = request.form
-    token = login(data['username'], data['password'])
+    user = get_user_by_name(data['name'])
+
+    if user and user.role != 'staff':
+        flash('Bad username or password given'), 401
+        return redirect(url_for('index_views.login_page'))
+
+    token = jwt_authenticate(data['name'], data['password'])
+    if not token:
+        flash('Bad username or password given'), 401
+        response = redirect(url_for('index_views.login_page'))
+    else:
+        flash('Login Successful')
+        response = redirect(url_for('index_views.staff_home_page'))
+        set_access_cookies(response, token) 
+    return response
+
+@auth_views.route('/login/student', methods=['POST'])
+def login_student():
+    data = request.form
+    user = get_user_by_name(data['name'])
+
+    if user and user.role != 'student':
+        flash('Bad username or password given'), 401
+        return redirect(url_for('index_views.login_page'))
+    
+    token = jwt_authenticate(data['name'], data['password'])
     response = redirect(request.referrer)
     if not token:
         flash('Bad username or password given'), 401
+        response = redirect(url_for('index_views.login_page'))
     else:
         flash('Login Successful')
+        response = redirect(url_for('index_views.student_home_page'))
         set_access_cookies(response, token) 
     return response
 
@@ -40,30 +54,5 @@ def login_action():
 def logout_action():
     response = redirect(request.referrer) 
     flash("Logged Out!")
-    unset_jwt_cookies(response)
-    return response
-
-'''
-API Routes
-'''
-
-@auth_views.route('/api/login', methods=['POST'])
-def user_login_api():
-  data = request.json
-  token = login(data['username'], data['password'])
-  if not token:
-    return jsonify(message='bad username or password given'), 401
-  response = jsonify(access_token=token) 
-  set_access_cookies(response, token)
-  return response
-
-@auth_views.route('/api/identify', methods=['GET'])
-@jwt_required()
-def identify_user():
-    return jsonify({'message': f"username: {current_user.username}, id : {current_user.user_id}"})
-
-@auth_views.route('/api/logout', methods=['GET'])
-def logout_api():
-    response = jsonify(message="Logged Out!")
     unset_jwt_cookies(response)
     return response
