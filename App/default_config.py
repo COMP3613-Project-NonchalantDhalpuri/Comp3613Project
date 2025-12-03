357c18f60291650e64e17a2ecce53308e0ef73cd
Richard <richardleonard4505@gmail.com>
2025-12-03 15:40:26 -0400

    Switch to Postgres env var + re-add /init route for DB initialization

--- App/default_config.py
+++ App/default_config.py
@@ -1,2 +1,8 @@
-SQLALCHEMY_DATABASE_URI="sqlite:///temp-database.db"
-SECRET_KEY="secret key"
+import os
+
+SQLALCHEMY_DATABASE_URI = os.environ.get(
+    "SQLALCHEMY_DATABASE_URI",  
+    "sqlite:///temp-database.db"  
+)
+
+SECRET_KEY = os.environ.get("SECRET_KEY", "secret key")
